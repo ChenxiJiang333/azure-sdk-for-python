6600e59698d4b2c8a964b830543f0d47f8321a4c
ChenxiJiang333 <v-chenjiang@microsoft.com>
2026-03-20 16:34:06 +0800

    regenerate from swagger

--- sdk/resources/azure-mgmt-resourcegraph/generated_samples/resources_summarize_query.py
+++ sdk/resources/azure-mgmt-resourcegraph/generated_samples/resources_summarize_query.py
@@ -16,7 +16,7 @@ from azure.mgmt.resourcegraph import ResourceGraphClient
     pip install azure-identity
     pip install azure-mgmt-resourcegraph
 # USAGE
-    python resources_history_mgs_get.py
+    python resources_summarize_query.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -31,16 +31,15 @@ def main():
         subscription_id="SUBSCRIPTION_ID",
     )
 
-    response = client.resources_history(
-        request={
-            "managementGroups": ["e927f598-c1d4-4f72-8541-95d83a6a4ac8", "ProductionMG"],
-            "options": {"interval": {"end": "2020-11-12T01:25:00.0000000Z", "start": "2020-11-12T01:00:00.0000000Z"}},
-            "query": "where name =~ 'cpu-utilization' | project id, name, properties",
+    response = client.resources(
+        query={
+            "query": "Resources | project id, name, type, location | summarize by location",
+            "subscriptions": ["cfbbd179-59d2-4052-aa06-9270a38aa9d6"],
         },
     )
     print(response)
 
 
-# x-ms-original-file: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/ResourceGraph/preview/2021-06-01-preview/examples/ResourcesHistoryMgsGet.json
+# x-ms-original-file: specification/resourcegraph/resource-manager/Microsoft.ResourceGraph/ResourceGraph/stable/2024-04-01/examples/ResourcesSummarizeQuery.json
 if __name__ == "__main__":
     main()
